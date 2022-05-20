d5f815c86047775ad54e55ef129150fd3aea82ce
DanaDanisova <Dana.Danisova@ibm.com>
2022-05-20 09:01:58 +0200

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const events = require("./data.js");
+const path = require("path");
 
 const app = express();
 
@@ -9,6 +10,15 @@ const app = express();
 app.get("/api/events", (req, res) => {
   res.status(200).send(JSON.stringify(events));
 });
+
+// Serve static assets
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server started on port ${port}.`));
